e2076e1b5b71a6eeccfb68d97a9a7c1cb779776d
hadespipe <hadespipe@gmail.com>
2017-10-26 17:21:16 +0700

    firebase init

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBGcUjGKwNZKlS45uVnqOo8mqFsSSz2v_k",
+    authDomain: "itts-1508472353306.firebaseapp.com",
+    databaseURL: "https://itts-1508472353306.firebaseio.com",
+    projectId: "itts-1508472353306",
+    storageBucket: "itts-1508472353306.appspot.com",
+    messagingSenderId: "694042003536"
+  };
+  firebase.initializeApp(config);
+</script>
